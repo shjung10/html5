a19a9f1014d751b5c09da8f4beab7fba59c9c1e4
shjung10 <cat7124@naver.com>
2025-06-26 15:49:47 +0900

    확대

--- gps/src/App.jsx
+++ gps/src/App.jsx
@@ -18,9 +18,9 @@ function App() {
 
   return (
     <div>
-      위도:{lat} <br />
-      경도:{lon} <br />
-      정확도:{accuracy} <br />
+      <h1>위도:{lat} </h1><br />
+      <h1>경도:{lon} </h1><br />
+      <h1>정확도:{accuracy} </h1><br />
     </div>
   )
 }
